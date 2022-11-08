6463506f48c6aee09ae25ed878cfb846ba0b8970
TEMBOAN <temboan@zra.org.zm>
2022-11-08 17:07:31 +0200

    create aggregated data ready for visualization

--- RedisTest/src/Frames.jl
+++ RedisTest/src/Frames.jl
@@ -4,6 +4,8 @@ using  DataFrames:select, groupby,DataFrame,combine,ByRow,nrow,subset,first,sort
 using Redis
 using Pipe
 using Dates
+
+
 function run()
 
     conn = RedisConnection() 
@@ -17,36 +19,62 @@ function run()
     obs = [split(line,"|") for line in lines]
 
     #@show features
-   obsMtx = mapreduce(permutedims,vcat, obs)
+    obsMtx = mapreduce(permutedims,vcat, obs)
 
-   columns = [col for col in eachcol(obsMtx)]
+    columns = [col for col in eachcol(obsMtx)]
 
+    df = DataFrame(columns, features)
 
+    #convert type
+    df[!,:sent_booking_request] = passmissing(parse).(Int16, df[!,:sent_booking_request])
+    df[!,:sent_message] = passmissing(parse).(Int16, df[!,:sent_message])
+    df[!,:ts_min] = passmissing(DateTime).(df[!,:ts_min], "yyyy-mm-dd HH:MM:SS")
+    df[!,:ts_max] = passmissing(DateTime).(df[!,:ts_max], "yyyy-mm-dd HH:MM:SS")
 
-  df = DataFrame(columns, features)
+    #group and count agent
+    group_and_count_agents = @time @pipe select(df,[:id_visitor,:dim_user_agent]) |> groupby(_,:dim_user_agent) |> combine(_,nrow => :count)  |> sort(_,:count,rev=true) |> first(_,20)
 
-  #convert type
-  df[!,:sent_booking_request] = passmissing(parse).(Int16, df[!,:sent_booking_request])
-  df[!,:sent_message] = passmissing(parse).(Int16, df[!,:sent_message])
-  df[!,:ts_min] = passmissing(DateTime).(df[!,:ts_min], "yyyy-mm-dd HH:MM:SS")
-  df[!,:ts_max] = passmissing(DateTime).(df[!,:ts_max], "yyyy-mm-dd HH:MM:SS")
+    #Top 5 clients
+    top_5_clients = @time @pipe select(df,[:id_visitor]) |> groupby(_,:id_visitor) |> combine(_,nrow => :count) |> sort(_,:count,rev=true) |> first(_,5)
 
-  @show names(df)
+    #Sent a message vs sent a booking request by date sample of 100 latest 
+    sent_messages_vs_sent_bookings_by_date = @time @pipe select(df,[:id_visitor,:sent_message,:sent_booking_request,:ds]) |> groupby(_,:ds) |> combine(_, :sent_booking_request => snt -> sum(snt),:sent_message => snt -> sum(snt)) |> sort(_,:ds, rev=true) |> first(_,100)
 
-  #group and count agent
-  @time @pipe select(df,[:id_visitor,:dim_user_agent]) |> groupby(_,:dim_user_agent) |> combine(_,nrow => :count)
+    #Sent a message vs sent
+    sent_messages_vs_sent_bookings = @time @pipe select(df,[:id_visitor,:sent_message,:sent_booking_request,:ds]) |> combine(_, :sent_booking_request => snt -> sum(snt),:sent_message => snt -> sum(snt))
 
-  #Top 5 clients
-  @time @pipe select(df,[:id_visitor]) |> groupby(_,:id_visitor) |> combine(_,nrow => :count) |> sort(_,:count,rev=true) |> first(_,5)
+    #Time spent per session
+    time_spent_per_session = @time @pipe select(df,:id_visitor,[:ts_min,:ts_max] => (a,b) -> convert.(Dates.Second, (b-a))) |> first(_,50)
 
-  #Sent a message vs sent a booking request by date sample of 100 latest 
-  @time @pipe select(df,[:id_visitor,:sent_message,:sent_booking_request,:ds]) |> groupby(_,:ds) |> combine(_, :sent_booking_request => snt -> sum(snt),:sent_message => snt -> sum(snt)) |> sort(_,:ds, rev=true) |> first(_,100)
+    @time writeDftoRedis(conn,"group_and_count_agents",group_and_count_agents)
+    @time writeDftoRedis(conn,"top_5_clients",top_5_clients)
+    @time writeDftoRedis(conn,"sent_messages_vs_sent_bookings_by_date",sent_messages_vs_sent_bookings_by_date)
+    @time writeDftoRedis(conn,"sent_messages_vs_sent_bookings",sent_messages_vs_sent_bookings)
+    @time writeDftoRedis(conn,"time_spent_per_session",time_spent_per_session)
 
-  #Sent a message vs sent
- @time @pipe select(df,[:id_visitor,:sent_message,:sent_booking_request,:ds]) |> combine(_, :sent_booking_request => snt -> sum(snt),:sent_message => snt -> sum(snt))
+    #readDffromRedis(conn,"group_and_count_agents",["dim_user_agent","count"])
+    #group_and_count_agents
+    time_spent_per_session
+end
 
- @time @pipe select(df,[:id_visitor,:ts_min,:ts_max]) |> transform(_,[:ts_min,:ts_max] => (a,b) -> convert.(Dates.Minu, (b-a)))
+function writeDftoRedis(conn,key::String,df::DataFrame)
+    for colName in names(df)
+        lpush(conn, "$key:$colName",df[:,colName])
+    end    
+end
 
+function readDffromRedis(conn,key::String,columns::Vector{String})
+    ds::Dict{String, Vector{String}} = Dict()
+    
+    length = llen(conn,"$key:$(columns[1])")
+
+    for colName in columns
+         data = lrange(conn, "$key:$colName",1,length)
+         ds[colName] = data
+    end 
+    
+    @show ds["dim_user_agent"][18]
+    @show ds["count"][18]
 end
     
 end
